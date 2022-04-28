f0890b36e3dfae6546b4a5ae32b928e0cde28c87
Nadir Kheiralla <48027451+VizaraVex@users.noreply.github.com>
2022-04-28 13:29:21 -0400

    Create JavaStringMethods.java

---
+++ JavaStringMethods.java
@@ -0,0 +1,23 @@
+/*
+  Author: Nadir Kheiralla
+  Title: JavaStringMethods
+  Description: Basic Java program used for practice. Basic string methods available in java.
+*/
+public class Review {
+  
+  public static void main(String[] args) {
+    String = myInfo = "An average man with an average mind.";
+    String = extraInfo = "I like to develop apps.";
+
+    System.out.println(myInfo.length());
+    System.out.println(myInfo.concat(extraInfo));
+    System.out.println(myInfo.indexOf(0));
+    System.out.println(myInfo.charAt(1));
+    System.out.println(myInfo.equals(extraInfo));
+    System.out.println(myInfo.substring(2, 9));
+    System.out.println(extraInfo.toUpperCase());
+    
+    
+  }
+  
+}
