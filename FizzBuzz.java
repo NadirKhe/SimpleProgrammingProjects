7bf7db0cb001f4ad17fca196990d02b35544df32
NadirK <48027451+VizaraVex@users.noreply.github.com>
2022-04-28 07:22:40 -0400

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,30 @@
+/*
+  Author: Nadir Kheiralla
+  Title: FizzBuzz Program
+  Description: Basic Java program used for practice on the top programming interview challenge
+*/
+public class FizzBuzz {
+
+  //Main method
+  public static void main(String[] args) {
+
+    //For loop which will output until 100
+    for (i = 1; i <= 100; i++) {
+
+      if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("FizzBuzz");
+      }
+      else if (i % 3 == 0) {
+        System.out.println("Fizz");
+      }
+      else if (i % 5 == 0) {
+        System.out.println("Buzz"); 
+      }
+      else {
+        System.out.println(i);
+      }
+      
+    }
+
+  }
+}
