c4e4e26c00350dc8ad3a223883643c87e5078ee2
Nadir Kheiralla <48027451+VizaVex@users.noreply.github.com>
2022-05-05 20:12:29 -0400

    Update KelvinWeather.js

--- JavaScriptProjects/KelvinWeather.js
+++ JavaScriptProjects/KelvinWeather.js
@@ -1,1 +1,12 @@
-console.log('Ogie Love, Ogie Life');
+/*
+  Author: Nadir Kheiralla
+  Title: Kelvin Weather App
+  Description: Basic JavaScript program used for practice on converting Kelvin to Celsuis which converts to Fahrenheit
+*/
+//Temperature of Kelvin
+const kelvin = 293;
+//Converting from Kelvin to Celsius
+var celsius = kelvin - 273;
+//Converting from Celsius to Fahrenheit
+let fahrenheit = Math.floor(celsius * (9/5) + 32);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
